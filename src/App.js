0d0f5947c7de84a8a873115717c364110f2ff899
adgaphani <phani@phanisetty.info>
2024-11-19 12:18:53 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FilePicker from "./components/FilePicker";
 
 function App() {
+  const handleFileSelect = (file) => {
+    console.log("Selected File:", file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Code Analysis Tool (CAT)</h1>
+      <FilePicker onFileSelect={handleFileSelect} />
     </div>
   );
 }
